6680bf7f688162296ee460106d5238098fe66472
Ivan Gorstkin <ivan.orlovsky.v@gmail.com>
2025-09-15 01:39:45 +0200

    Adjust color scheme

---
+++ src/app/primeng-theme/button.ts
@@ -0,0 +1,21 @@
+import { ButtonDesignTokens } from '@primeng/themes/aura/button'
+
+export const primengThemeButton: ButtonDesignTokens = {
+  colorScheme: {
+    light: {
+      root: {
+        primary: {
+          // activeBackground: '',
+          focusRing: {
+            // color: '',
+          },
+        },
+      },
+      text: {
+        primary: {
+          // activeBackground: '',
+        },
+      },
+    },
+  },
+}
--- src/app/primeng-theme/index.ts
+++ src/app/primeng-theme/index.ts
@@ -2,8 +2,11 @@ import { definePreset } from '@primeng/themes'
 import Aura from '@primeng/themes/aura'
 import { Preset } from '@primeuix/themes/types'
 import { AuraBaseDesignTokens } from '@primeng/themes/aura/base'
+import { primengThemeButton } from './button'
 
 export const dndPrimeNgTheme = definePreset(Aura, {
   semantic: {},
-  components: {},
+  components: {
+    button: primengThemeButton,
+  },
 } satisfies Preset<AuraBaseDesignTokens>)
