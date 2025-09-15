ff7d97ce8f569880c34479c51ec104a141da9c94
Ivan Gorstkin <ivan.orlovsky.v@gmail.com>
2025-09-15 02:28:55 +0200

    Set color scheme for secondary icon only buttons

--- src/app/primeng-theme/button.ts
+++ src/app/primeng-theme/button.ts
@@ -3,17 +3,25 @@ import { ButtonDesignTokens } from '@primeng/themes/aura/button'
 export const primengThemeButton: ButtonDesignTokens = {
   colorScheme: {
     light: {
+      text: {
+        secondary: {
+          color: 'var(--dnd-color-text-primary)',
+          hoverBackground: 'var(--dnd-color-background-backdrop)',
+          activeBackground: 'var(--dnd-color-background-backdrop-active)',
+        },
+      },
+    },
+    dark: {
       root: {
-        primary: {
-          // activeBackground: '',
-          focusRing: {
-            // color: '',
-          },
+        secondary: {
+          color: 'var(--dnd-color-text-primary)',
         },
       },
       text: {
-        primary: {
-          // activeBackground: '',
+        secondary: {
+          color: 'var(--dnd-color-text-primary)',
+          hoverBackground: 'var(--dnd-color-background-backdrop)',
+          activeBackground: 'var(--dnd-color-background-backdrop-active)',
         },
       },
     },
