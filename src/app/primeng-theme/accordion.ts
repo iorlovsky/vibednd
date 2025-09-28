4593a18ba456d85de8fd42bce3ad2f1fe95eb4a5
Ivan Gorstkin <ivan.orlovsky.v@gmail.com>
2025-09-28 20:46:03 +0200

    Improve basic styles for accordion

--- src/app/primeng-theme/accordion.ts
+++ src/app/primeng-theme/accordion.ts
@@ -2,7 +2,45 @@ import { AccordionDesignTokens } from '@primeng/themes/aura/accordion'
 
 export const primengThemeAccordion: AccordionDesignTokens = {
   colorScheme: {
-    light: {},
-    dark: {},
+    light: {
+      header: {
+        background: 'var(--dnd-color-primary)',
+        hoverBackground: 'var(--dnd-color-primary-hover)',
+        activeBackground: 'var(--dnd-color-primary)',
+        activeHoverBackground: 'var(--dnd-color-primary)',
+        color: 'var(--dnd-color-text-secondary)',
+        hoverColor: 'var(--dnd-color-text-secondary)',
+        activeColor: 'var(--dnd-color-text-secondary)',
+        activeHoverColor: 'var(--dnd-color-text-secondary)',
+      },
+      panel: {
+        borderWidth: '16px',
+        borderColor: 'transparent',
+      },
+      content: {
+        background: 'transparent',
+        padding: '0',
+      },
+    },
+    dark: {
+      header: {
+        background: 'var(--dnd-color-background-backdrop)',
+        hoverBackground: 'var(--dnd-color-background-backdrop-active)',
+        activeBackground: 'var(--dnd-color-background-backdrop)',
+        activeHoverBackground: 'var(--dnd-color-background-backdrop)',
+        color: 'var(--dnd-color-text-additional)',
+        hoverColor: 'var(--dnd-color-text-additional)',
+        activeColor: 'var(--dnd-color-text-additional)',
+        activeHoverColor: 'var(--dnd-color-text-additional)',
+      },
+      panel: {
+        borderWidth: '16px',
+        borderColor: 'transparent',
+      },
+      content: {
+        background: 'transparent',
+        padding: '0',
+      },
+    },
   },
 }
